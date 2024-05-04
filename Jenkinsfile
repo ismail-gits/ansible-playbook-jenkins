3937beaec62f1bf2e81e9198c164eb7f5f9fb56c
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-05-05 03:50:12 +0530

    Added script.groovy

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,18 +9,15 @@ pipeline {
         stage("copy to ansible server") {
             steps {
                 script {
-                    echo "copying all necessary files to ansible control node server"
-                    sshagent(['ansible-server-key']) {
-                        sh "scp -o StrictHostKeyChecking=no ansible/* ec2-user@13.200.237.65:~/"
+                    gv.copyToAnsible()
+                }
+            }
+        }
 
-                        withCredentials([sshUserPrivateKey(
-                            credentialsId: 'ec2-server-key',
-                            keyFileVariable: 'keyfile',
-                            usernameVariable: 'user'
-                        )]) {
-                                sh 'scp $keyfile ec2-user@13.200.237.65:~/ansible-jenkins.pem'
-                        }
-                    }
+        stage("execute ansible playbook") {
+            steps {
+                script {
+                    gv.executeAnsiblePlaybook()
                 }
             }
         }
--- script.groovy
+++ script.groovy
@@ -1,3 +1,37 @@
 def copyToAnsible() {
-    
+    echo "copying all necessary files to ansible control node server"
+
+    sshagent(['ansible-server-key']) {
+        sh "scp -o StrictHostKeyChecking=no ansible/* ec2-user@13.200.237.65:~/"
+
+        withCredentials([sshUserPrivateKey(
+            credentialsId: 'ec2-server-key',
+            keyFileVariable: 'keyfile',
+            usernameVariable: 'user'
+        )]) {
+                sh 'scp $keyfile ec2-user@13.200.237.65:~/ansible-jenkins.pem'
+        }
+    }
 }
+
+def executeAnsiblePlaybook() {
+    echo "executing ansible playbook to configure ec2 instances"
+
+    def remote = [:]
+    remote.name = "ansible-server"
+    remote.host = "13.200.237.65"
+    remote.allowAnyHosts = true
+
+    withCredentials([sshUserPrivateKey(
+        credentialsId: 'ansible-server-key',
+        keyFileVariable: 'keyfile',
+        usernameVariable: 'user'
+    )]) {
+        remote.user = user
+        remote.identityFile = keyfile
+
+        sshCommand remote: remote, command: "ls -l"
+    }
+}
+
+return this
