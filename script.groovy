d2d55338ad83ffe9904ec8757719ca309094119d
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-05-05 04:00:58 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,21 @@
+def copied = false
+
 def copyToAnsible() {
     echo "copying all necessary files to ansible control node server"
 
     sshagent(['ansible-server-key']) {
         sh "scp -o StrictHostKeyChecking=no ansible/* ec2-user@13.200.237.65:~/"
 
-        withCredentials([sshUserPrivateKey(
+        if (!copied) {
+            withCredentials([sshUserPrivateKey(
             credentialsId: 'ec2-server-key',
             keyFileVariable: 'keyfile',
             usernameVariable: 'user'
-        )]) {
+            )]) {
                 sh 'scp $keyfile ec2-user@13.200.237.65:~/ansible-jenkins.pem'
+            }
+
+            copied = true
         }
     }
 }
